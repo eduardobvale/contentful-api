b4b1ca4af5bcb647fc68116f155c69a7059a58ad
Eduardo <eduardo@sonatalab.com>
2020-06-13 21:52:42 -0300

    Setting a low level caching of 12 hours for both API calls (no specific invalidation strategy yet)

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,10 +1,15 @@
 class RecipesController < ApplicationController
   def index
-    @recipes = helpers.contentful.entries(content_type: "recipe")
+    @recipes = Rails.cache.fetch("all/recipes", expires_in: 12.hours) do
+      helpers.contentful.entries(content_type: "recipe")
+    end
   end
 
   def show
-    @recipe = helpers.contentful.entries(content_type: "recipe", "sys.id" => params[:id]).first
+    @recipe = Rails.cache.fetch("#{params[:id]}/recipes", expires_in: 12.hours) do
+      helpers.contentful.entries(content_type: "recipe", "sys.id" => params[:id]).first
+    end
+
     render :not_found, status: :not_found if @recipe.nil?
   end
 end
